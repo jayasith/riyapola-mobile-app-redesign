3c2ae00851d782a44ebee6ca45f996c48600e499
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-09-25 19:20:56 +0530

    CREATE: common form component created

--- app/screens/NewListingScreen.js
+++ app/screens/NewListingScreen.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import { StatusBar, StyleSheet, Text } from "react-native";
-import { Formik } from "formik";
+import { StatusBar, StyleSheet } from "react-native";
 import * as Yup from "yup";
 
 import CategoryPicker from "../components/pickers/CategoryPicker";
+import Form from "../components/forms/Form";
+import FormInputWithError from "../components/inputs/FormInputWithError";
 import SubmitButton from "../components/buttons/SubmitButton";
 import TitleText from "../components/texts/TitleText";
 
 import categories from "../config/categories";
 import colors from "../config/colors";
-import FormInputWithError from "../components/inputs/FormInputWithError";
 
 const validationSchema = Yup.object().shape({
 	title: Yup.string().required().label("Title"),
@@ -22,7 +22,7 @@ const NewListingScreen = () => {
 	const [category, setCategory] = useState(categories[0]);
 
 	return (
-		<Formik
+		<Form
 			initialValues={{
 				title: "",
 				price: "",
@@ -33,38 +33,34 @@ const NewListingScreen = () => {
 			onSubmit={(values) => console.log(values)}
 			validationSchema={validationSchema}
 		>
-			{() => (
-				<>
-					<StatusBar backgroundColor={colors.white} barStyle="dark-content" />
-					<TitleText style={styles.title}>New Listing</TitleText>
-					<FormInputWithError placeholder="Title " icon="create" name="email" />
-					<FormInputWithError
-						placeholder="Price "
-						icon="attach-money"
-						keyboardType="decimal-pad"
-						name="price"
-					/>
-					<CategoryPicker
-						selectedItem={category}
-						onSelectItem={(category) => setCategory(category)}
-						placeholder="Category "
-						icon="apps"
-						categories={categories}
-					/>
-					<FormInputWithError
-						placeholder="Description "
-						icon="subtitles"
-						name="description"
-					/>
-					<FormInputWithError
-						placeholder="Location "
-						icon="location-on"
-						name="location"
-					/>
-					<SubmitButton title="Save" style={{ width: "90%", top: 20 }} />
-				</>
-			)}
-		</Formik>
+			<StatusBar backgroundColor={colors.white} barStyle="dark-content" />
+			<TitleText style={styles.title}>New Listing</TitleText>
+			<FormInputWithError placeholder="Title " icon="create" name="email" />
+			<FormInputWithError
+				placeholder="Price "
+				icon="attach-money"
+				keyboardType="decimal-pad"
+				name="price"
+			/>
+			<CategoryPicker
+				selectedItem={category}
+				onSelectItem={(category) => setCategory(category)}
+				placeholder="Category "
+				icon="apps"
+				categories={categories}
+			/>
+			<FormInputWithError
+				placeholder="Description "
+				icon="subtitles"
+				name="description"
+			/>
+			<FormInputWithError
+				placeholder="Location "
+				icon="location-on"
+				name="location"
+			/>
+			<SubmitButton title="Save" style={{ width: "90%", top: 20 }} />
+		</Form>
 	);
 };
 
