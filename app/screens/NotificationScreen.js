d520af9a37dcf9972e106d41421e6b27f77b3738
OnellaNatalie <onellanatalie824@gmail.com>
2021-09-29 10:40:39 +0530

    Settings switch
    Notification card, switch,

--- app/screens/NotificationScreen.js
+++ app/screens/NotificationScreen.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, StatusBar, StyleSheet , ScrollView} from "react-native";
+import { View, StatusBar, StyleSheet , ScrollView, Text} from "react-native";
 import TitleText from "../components/texts/TitleText";
 import SubtitleText from "../components/texts/SubtitleText";
 import ParagraphText from "../components/texts/ParagraphText";
 import SubTopic from "../components/texts/SubTopic";
 import colors from "../config/colors";
 import { Ionicons, Entypo} from "@expo/vector-icons";
+import NotificationCard from "../components/cards/NotificationCard";
 import TextWithButton from "../components/inputs/TextwithButton";
 const NotificationScreen = () => {
   return (
@@ -14,13 +15,14 @@ const NotificationScreen = () => {
       <StatusBar backgroundColor={colors.white} barStyle="dark-content" />
       <TitleText style={styles.title}>Notification</TitleText>
 
-      <SubTopic style={styles.text}>text</SubTopic>
-      <SubTopic style={styles.text}>text</SubTopic>
-      <SubTopic style={styles.text}>text</SubTopic>
-      <SubTopic style={styles.text}>text</SubTopic>
-      <SubTopic style={styles.text}>text</SubTopic>
-      <TextWithButton title="Settings" icon="cog" />
+
+       <NotificationCard title=" Successfully change the password" time="1 min ago"/>
+           <NotificationCard title=" Successfully change the email" time="1 min ago"/>
     </View>
+    <ParagraphText style={{ color: colors.primary, left:350 }}>
+						clear
+					</ParagraphText>
+
     </ScrollView>
   );
 };
@@ -30,5 +32,7 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
     left: 150,
   },
+    
+
 });
 export default NotificationScreen;
